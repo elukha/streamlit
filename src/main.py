7d70b6ba001c0ababebc3ee4ac074f5cfd5e49bf
elukha <150355353+elukha@users.noreply.github.com>
2024-02-25 12:32:17 +0900

    完成

--- src/main.py
+++ src/main.py
@@ -9,19 +9,18 @@ page = st.sidebar.selectbox("ページを選択してください", ["入力", "
 
 price = []
 date = []
-all_data = []
-a=[[]]
+a = []
+
+# cache化するため
+@st.cache_resource
+def all_cache_lst():
+    lst = []
+    return lst
+a = all_cache_lst()
+
 # 選択されたページを表示
 if page == "入力":
     st.title("入力したデータをグラフにする")
-
-    # cache化するため
-    @st.cache_resource
-    def all_cache_lst():
-        lst = []
-        return lst
-
-    all_data = all_cache_lst()
 
     min_date = datetime.date(1900, 1, 1)
     max_date = datetime.date(2100, 1, 1)
@@ -33,31 +32,27 @@ if page == "入力":
         month = d.month
         day = d.day
         edit_date = f"{year}-{month}-{day}"
-        b = [edit_date, place]
+        b = [place, edit_date]
+        print(b)
         a.append(b)
         print(a)
-        
-        
-        
-        st.write(all_data)
+        st.write(a)
 
     # 入力した要素をクリアする。
     def remove_data():
-        price.clear()
-        date.clear()
+        a.clear()
 
 
     place = st.number_input("価格を入力してください", 0, step=300)
     st.button("追加", on_click=lambda :add_data(place, d))
     st.button("クリア", on_click=remove_data)
 
-
 elif page == "グラフ出力":
-    hoge=[['12/1','9999'],['12/5','11111']]
+    print(a)
     data = pd.DataFrame(
-        hoge,columns=['日付','値段']
+        a,columns=['値段', '日付']
     )
     print(data)
     st.write('折れ線グラフ:')
     
-    st.line_chart(data.set_index("日付"))
+    st.line_chart(data.set_index("日付"), use_container_width=True)
